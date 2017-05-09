b9fdd428aca75c3649722253c9a85ed3ea55ae72
Rich Friedel <rich.friedel@willowtreeapps.com>
2017-05-09 18:40:27 -0400

    Add project code

---
+++ app/src/main/java/net/rf43/androidthingsiot_lightbulbs/LightBulbsApp.java
@@ -0,0 +1,11 @@
+package net.rf43.androidthingsiot_lightbulbs;
+
+import android.app.Application;
+
+public class LightBulbsApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
