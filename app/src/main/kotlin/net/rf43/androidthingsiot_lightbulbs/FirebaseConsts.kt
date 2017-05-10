09ac20961bca0c02e51248367463438a0fda02c0
Rich Friedel <rich.friedel@willowtreeapps.com>
2017-05-09 21:15:07 -0400

    Convert main package to using Kotlin Remove java package from main and replace with kotlin packages

---
+++ app/src/main/kotlin/net/rf43/androidthingsiot_lightbulbs/FirebaseConsts.kt
@@ -0,0 +1,11 @@
+package net.rf43.androidthingsiot_lightbulbs
+
+internal val KEY_LIGHT_STATE = "lightState"
+internal val KEY_STATE = "state"
+
+internal val KEY_LIGHT_RED = "light01"
+internal val KEY_LIGHT_GREEN = "light02"
+internal val KEY_LIGHT_BLUE = "light03"
+
+internal val VALUE_ON = "on"
+internal val VALUE_OFF = "off"
---
+++ app/src/main/kotlin/net/rf43/androidthingsiot_lightbulbs/LightBulbsApp.kt
@@ -0,0 +1,10 @@
+package net.rf43.androidthingsiot_lightbulbs
+
+import android.app.Application
+
+class LightBulbsApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
